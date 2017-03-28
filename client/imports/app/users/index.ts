5bd568a2f94a9a0cd34ed173f47f0ce14ad17a3b
MohmadAyman <no.future2e1@gmail.com>
2017-03-28 06:53:49 +0200

    removed conf booking

--- client/imports/app/users/index.ts
+++ client/imports/app/users/index.ts
@@ -3,12 +3,10 @@ import { ClassesListComponent } from './classes-list.component';
 import { ThanksComponent } from './thanks.component';
 import { AvilableClasses } from './active-classes.component';
 import { TutorDetailsComponentUser } from './tutor-details.component';
-import { ConfirmBooking } from './confirm-booking.component';
 export const USER_DECLARATIONS = [
   AddClassUserFormComponent,
   ClassesListComponent,
   ThanksComponent,
   AvilableClasses,
-  TutorDetailsComponentUser,
-  ConfirmBooking
+  TutorDetailsComponentUser
 ];
